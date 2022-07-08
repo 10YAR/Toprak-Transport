2078453f0c91c398167a830aaf56f3260f6cc61f
Diyar <diyar@digiplace.fr>
2022-07-08 18:02:29 +0200

    CGU

---
+++ cgu.php
@@ -0,0 +1,54 @@
+<?php require_once "core/header.php"; ?>
+<?php require_once "core/navigation.php"; ?>
+    <section class="tj-contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="tj-heading-style">
+                    <h3>Conditions générales d'utilisation</h3>
+                </div>
+                <br/>
+                <div class="col-md-12 col-sm-8">
+                    <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Center;"><br><br>Les présentes conditions générales régissent l’utilisation de ce site <span
+                                style="font-style:italic;font-weight:normal;">toprak-transport.fr</span>.<br>Ce site appartient et est géré par Bulent BAYRAKLI<br>En utilisant ce site, vous indiquez que vous avez lu et compris les conditions d’utilisation et que vous acceptez de les respecter en tout temps.<br><br>Type de site : vitrine
+                    </p>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Propriété intellectuelle</span><br><br>Tout contenu publié et mis à disposition sur ce site est la propriété de
+                            Bulent BAYRAKLI et de ses créateurs. Cela comprend, mais n’est pas limité aux images, textes, logos, documents, fichiers téléchargeables et tout ce qui contribue à la composition de ce site.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Restrictions d'âge</span><br><br>L’âge minimum pour utiliser ce site est de 15 ans. Les utilisateurs conviennent
+                            qu’en utilisant notre site, ils ont plus de 15 ans. Nous n’assumons aucune responsabilité juridique pour les fausses déclarations sur l’âge.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Limitation de responsabilité</span><br><br>Bulent BAYRAKLI ou l’un de ses employés sera tenu responsable de tout
+                            problème découlant de ce site. Néanmoins, Bulent BAYRAKLI et ses employés ne seront pas tenus responsables de tout problème découlant de toute utilisation irrégulière de ce site.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Indemnité</span><br><br>En tant qu’utilisateur, vous indemnisez par les présentes Bulent BAYRAKLI de toute
+                            responsabilité, de tout coût, de toute cause d’action, de tout dommage ou de toute dépense découlant de votre utilisation de ce site ou de votre violation de l’une des dispositions énoncées dans le présent document.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Lois applicables</span><br><br>Ce document est soumis aux lois applicables en France et vise à se conformer à ses
+                            règles et règlements nécessaires. Cela inclut la réglementation à l’échelle de l’UE énoncée dans le RGPD.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Divisibilité</span><br><br>Si, à tout moment, l’une des dispositions énoncées dans le présent document est jugée
+                            incompatible ou invalide en vertu des lois applicables, ces dispositions seront considérées comme nulles et seront retirées du présent document. Toutes les autres dispositions ne seront pas touchées par les lois et le reste du document sera toujours considéré comme valide.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Modifications</span><br><br>Ces conditions générales peuvent être modifiées de temps à autre afin de maintenir le
+                            respect de la loi et de refléter tout changement à la façon dont nous gérons notre site et la façon dont nous nous attendons à ce que les utilisateurs se comportent sur notre site. Nous recommandons à nos utilisateurs de vérifier ces conditions générales de temps à autre pour s’assurer qu’ils sont informés de
+                            toute mise à jour. Au besoin, nous informerons les utilisateurs par courriel des changements apportés à ces conditions ou nous afficherons un avis sur notre site.
+                        </p></div>
+                    <div>
+                        <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Left;"><span style="font-style:normal;font-weight:bold;text-decoration:underline;">Contact</span><br><br>Veuillez communiquer avec nous si vous avez des questions ou des préoccupations. Nos
+                            coordonnées sont les suivantes :<br><br>06 59 74 26 84<br><br>contact@toprak-transport.fr<br><br>1 rue d'Otley, 77130, Montereau-Fault-Yonne
+                        </p></div>
+                    <p style="line-height:18.0pt;font-size:12.0pt;line-height:18.0pt;color:#000000;text-align:Right;"><span style="font-style:italic;font-weight:normal;">Date d'entrée en vigueur : </span>le 8 juillet 2022.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php require_once "core/bandeau-footer.php"; ?>
+<?php require_once "core/footer.php"; ?>
