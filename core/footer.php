a93a619e694cb8de897debf89da496ec22b0f22d
Diyar <diyar@digiplace.fr>
2022-07-10 23:29:14 +0200

    fix css

--- core/footer.php
+++ core/footer.php
@@ -14,7 +14,7 @@
                     <p>
                         &copy; Copyright <?= date("Y") ?> Toprak Transport - Taxi en Seine-et-Marne<br />
                         <a href="/mentions-legales.php">Mentions légales</a> - <a href="/cgu.php">CGU</a> - <a href="/confidentialite.php">Politique de confidentialité</a>
-                        <span style="float:right;display:inline-block;position:absolute;top:-5px;right:0;">Horaires d'ouverture : 7j/7 et 24h/24 <br/> Réalisation <a href="https://diyar-bayrakli.fr" target="_blank">Diyar Bayrakli</a></span>
+                        <span class="footer_leftText">Horaires d'ouverture : 7j/7 et 24h/24 <br/> Réalisation <a href="https://diyar-bayrakli.fr" target="_blank">Diyar Bayrakli</a></span>
                     </p>
                 </div>
             </div>
