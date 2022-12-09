a2d8318052af58b7220200948f9883024a8fa525
Diyar <diyar@digiplace.fr>
2022-12-09 11:09:58 +0100

    prices adjustments

--- xhr.calculate_trip.php
+++ xhr.calculate_trip.php
@@ -86,7 +86,7 @@ function calculatePrice($depart, $arrivee, $pick_date, $pick_time, $allerretour)
     }
 
     // Définition des tranches de prix supplémentaires
-    $tranches = array("O" => 0, "A" => 15, "B" => 20, "C" => 30, "D" => 40);
+    $tranches = array("O" => 7, "A" => 15, "B" => 20, "C" => 30, "D" => 40);
 
     // Si réservation de nuit... prix plus cher!
     if ($pick_hour > 23 OR $pick_hour < 7)
